bde7f86a311af83dce80929e7494ade40ab46bb5
kcustod1 <kaikane.custodio@wsu.edu>
2023-10-24 00:33:38 -0700

    Made a little guy walk around.

---
+++ Creatures/Player.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 400;
+
+	public Vector2 ArenaSize; //Size of the module
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ArenaSize = GetViewportRect().Size;
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+
+		if(Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+		if(Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+		if(Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+		if(Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		AnimatedSprite2D animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(x: Mathf.Clamp(Position.X, 0, ArenaSize.X), y: Mathf.Clamp(Position.Y, 0, ArenaSize.Y));
+
+	}
+}
