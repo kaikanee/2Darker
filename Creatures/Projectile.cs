ee30769bdbc3853a119a9c6f837a794e44987f1f
kcustod1 <kaikane.custodio@wsu.edu>
2023-10-24 01:33:57 -0700

    Added basic code for projectile scene. Still need to make player spawn the projectiles.

---
+++ Creatures/Projectile.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public partial class Projectile : Area2D
+{
+
+	[Export]
+	
+	//projectile speed
+	public int Speed {get; set;	} = 650;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+		// this is just stolen from the player class, just sends the velocity in the direction dictated by the movement input.
+		Vector2 velocity = Vector2.Zero;
+		if(Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+		if(Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+		if(Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+		if(Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+		Position += velocity * (float)delta;
+		// for now, the projectile is only deleted on projectile timeout, but eventually we will implement logic for it hitting a wall, etc. probably clamp it to screensize.
+	}
+
+	private void OnProjectileTimeout()
+	{
+		QueueFree(); //delete the bullet whenever its life is over
+	}
+
+
+
+	/// <summary>
+	/// Called when a body enters the projectile's hitbox. Basically when it hit something.
+	/// </summary>
+	/// <param name="body">The actual thing it hits.</param>
+	private void OnBodyEntered(Node2D body)
+	{
+		if(body.IsInGroup("enemy"))
+		{
+			// do something to the enemy,, maybe cast it and call a function damage() or something that takes away hp?
+		}
+		QueueFree(); // delete the bullet
+	}
+
+}
